0dec76b89b0ba1a1d737a3a8d9930dfab016554e
Christian Romney <cromney@pointslope.com>
2017-03-15 09:43:51 -0400

    Add wahwah
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- src/cljs/trumpet/sound.cljs
+++ src/cljs/trumpet/sound.cljs
@@ -2,24 +2,23 @@
   (:require [tonejs]
             [cljs.pprint :refer [pprint]]))
 
-(def config
-  (clj->js
-   {:oscillator {:type "pwm" :modulationFrequency 0.5}
-    :envelope {:attack 2.0
-               :decay 0.025
-               :sustain 0.25
-               :release 0.9}}))
+(defonce wahwah
+  (.toMaster (js/Tone.AutoWah. 58.27 6 -30)))
 
 (defonce synthesizer
-  (.toMaster (js/Tone.Synth. config)))
+  (.toMaster (js/Tone.AMSynth.)))
+
+(defonce output
+  (-> synthesizer
+      (.connect wahwah)))
 
 (def duration
   "Map of semantic duration names to values
   understood by the synthesizer."
-  {:whole-note "1n"
-   :half-note "2n"
+  {:whole-note   "1n"
+   :half-note    "2n"
    :quarter-note "4n"
-   :eigth-note "8n"})
+   :eigth-note   "8n"})
 
 (defn play
   "Plays a note in a given octave for the specified duration."
@@ -28,7 +27,7 @@
 
   ([octave-note time]
    (js/console.log "play: " (clj->js [octave-note time]))
-   (.triggerAttackRelease synthesizer octave-note time)))
+   (.triggerAttackRelease output octave-note time)))
 
 (defn octave
   "Converts a keyword note and integer octave into
